6e35fd086389818be9246b3dfc9b15058d8d4a0b
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-07-20 20:07:16 -0500

    In the Past
    All hour blocks in the past are now a displayed as such and unedittable

--- libs/main.js
+++ libs/main.js
@@ -72,7 +72,77 @@ var saveBtn = $('.saveBtn');
 //   });
 
 saveBtn.on('click', function () {
+    console.log('Local Storage (Key then Pair):')
     console.log($(this).siblings('.col-2').text());
     console.log($(this).siblings('.col-8').children().val());
     localStorage.setItem(($(this).siblings('.col-2').text()), ($(this).siblings('.col-8').children().val()));
 });
+
+function persistancePls() {
+    
+    if (localStorage.getItem('0900')) {
+        $('#0900').children('.col-8').children().val(localStorage.getItem('0900'));
+    }
+
+    if (localStorage.getItem('1000')) {
+        $('#1000').children('.col-8').children().val(localStorage.getItem('1000'));
+    }
+
+    if (localStorage.getItem('1100')) {
+        $('#1100').children('.col-8').children().val(localStorage.getItem('1100'));
+    }
+
+    if (localStorage.getItem('1200')) {
+        $('#1200').children('.col-8').children().val(localStorage.getItem('1200'));
+    }
+
+    if (localStorage.getItem('1300')) {
+        $('#1300').children('.col-8').children().val(localStorage.getItem('1300'));
+    }
+
+    if (localStorage.getItem('1400')) {
+        $('#1400').children('.col-8').children().val(localStorage.getItem('1400'));
+    }
+
+    if (localStorage.getItem('1500')) {
+        $('#1500').children('.col-8').children().val(localStorage.getItem('1500'));
+    }
+
+    if (localStorage.getItem('1600')) {
+        $('#1600').children('.col-8').children().val(localStorage.getItem('1600'));
+    }
+
+    if (localStorage.getItem('1700')) {
+        $('#1700').children('.col-8').children().val(localStorage.getItem('1700'));
+    }
+}
+
+const fakeHours = 14;
+
+function rowResponse() {
+    // console.log((moment().hours()))
+    var numMakeOld = (fakeHours - 9); //2
+    // console.log($('.container-fluid').children());
+    for (let i = 0; i < numMakeOld; i++) {
+        var rowToChange = $('.container-fluid').children().eq(i);
+        rowToChange.addClass('rowinthePast');
+            var colToChangeLeft = rowToChange.children().eq(0);
+            colToChangeLeft.addClass('colinthePast');
+            var colToChangeRight = rowToChange.children().eq(2);
+            colToChangeRight.addClass('colinthePast');
+            colToChangeRight.children().removeClass('pulse');
+            var colToChangeMid = rowToChange.children().eq(1);
+            colToChangeMid.addClass('col8inthePast');
+            colToChangeMid.children().removeClass('pulse');
+            colToChangeMid.children('textarea').attr('readonly', 'true');
+    }
+};
+
+function startUp() {
+    persistancePls();
+    rowResponse();
+};
+
+document.onload = startUp();
+
+//remember .eval
