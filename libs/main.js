4896a3a24b686c78228b0bf0817dedce7b069268
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-07-26 23:20:51 -0500

    Clean Up
    Code is more read able

--- libs/main.js
+++ libs/main.js
@@ -14,69 +14,11 @@ timetoAdd = setInterval(function () {
 }, 1000);
 
 
-
-
 //Set the date
 var currentDate = moment().format('ddd MM, YYYY')
 $("#currentDay").text(currentDate);
 
-//I could update the date but... for this assignment seems overkill
-// var timetoAdd;
-// timetoAdd = setInterval(function () {
-//     currentTime = moment().format('hh:mm:ss');
-//     $("#timeClock").text(currentTime);
-// }, 1000);
-
-//DEBUG KEYS
-document.addEventListener("keydown", keydownAction);
-function keydownAction(e) {
-    // if (e.altKey && e.key == '`') {     //Fire off the Debug Quiz
-    //     quizAsset = tstDEBUGFile.tstDEBUG;
-    //     loadQuiz(quizAsset);
-    // }
-    // if (e.altKey && e.key == 'm') {     //Jump back to Mainpage
-    //     console.log('Quit to Mainpage')
-    //     timerUI.style.visibility = "hidden";
-    //     resettoMain();
-    // }
-    if (e.altKey && e.key == 't') {     //Sets timer to 1PM to test functionality
-        console.log('DEBUG ~ Set time to 1PM');
-        clearInterval(timetoAdd)
-        currentTime = moment().hour(13).format('HH:mm:ss');
-        $("#timeClock").text(currentTime);
-    }
-}
-
-// var saveBtn = document.querySelectorAll(saveBtn);
-
-// function addListeners() {
-//     saveBtn.addEventListener('click', saveInfo);
-// }
-
-// document.onload = addListeners();
-
-// function saveInfo(e) {
-//     console.log("Hello");
-// }
-
 var saveBtn;
-
-// saveBtn.on('click', function () {        //Me learning jQuery DOM traversal
-//     console.log('This object:');
-//     console.log(this);
-//     console.log('The parent ');
-//     console.log($(this).parent()); //gotta wrap in a $() to use .parent()
-//     console.log('The siblings ');
-//     console.log($(this).siblings());
-//     console.log('The middle column ');
-//     console.log($(this).siblings('.col-8'));
-//     console.log('The textarea ');
-//     console.log($(this).siblings('.col-8').children());
-//     console.log('textarea value ');
-//     console.log($(this).siblings('.col-8').children().val()); //Got it :)
-
-//     console.log($(this).siblings('.col-2').text()); //And this gets the time
-//   });
 
 function updateBtns() {
 
@@ -131,12 +73,10 @@ function persistancePls() {
     }
 }
 
-// use moment().set({"hour": 11, "minute": 59}).format('HH:mm') or whatever for setting the time for debugging
-
 function rowResponse() {
-    // console.log((moment().hours()))
+
     var numMakeOld = (moment().format('HH') - 09);
-    // console.log($('.container-fluid').children());
+    
     for (let i = 0; i < numMakeOld; i++) {
         var rowToChange = $('.container-fluid').children().eq(i);
         rowToChange.addClass('rowinthePast');
@@ -181,5 +121,3 @@ function startUp() {
 };
 
 document.onload = startUp();
-
-//remember .eval
